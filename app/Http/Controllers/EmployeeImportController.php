57750034b11f86226ee325d10fdd950248b8b3ca
vamsiss4u <vamsikrishna@softsolutions4u.info>
2022-04-22 13:54:37 +0530

    employeeImportcsv

--- app/Http/Controllers/EmployeeImportController.php
+++ app/Http/Controllers/EmployeeImportController.php
@@ -29,24 +29,31 @@ class EmployeeImportController extends Controller
      */
     public function importEmployee(Request $request)
     {
+        $request->validate(
+            [
+                'file' => ['required', 'mimes:csv,txt'],
+            ]
+        );
         $fileName = time() . '.' . $request->file->getClientOriginalExtension();
         $request->file->move(public_path('uploads'), $fileName);
         $csvFile   = public_path('uploads/' . $fileName);
         $employees = $this->csvToArray($csvFile);
         // dd($employeeArr);
         $user = User::find(Auth::user()->id);
-        
+
         foreach ($employees as $employee) {
             $user->employee()->create([
-                'username'    => $employee['username'],
+                'username'    =>  ' ' ? $employee['email']: $employee['username'],
+                'employeeId'  => $employee['employeeId'],
                 'first_name'  => $employee['first_name'],
                 'last_name'   => $employee['last_name'],
+                'email'       => $employee['email'],
                 'department'  => $employee['department'],
                 'designation' => $employee['designation'],
-                'password'    => Hash::make($employee['password']),
+                'password'    =>  ' ' ? Hash::make($employee['email']):Hash::make($employee['password']),
             ]);
         }
         return redirect('employee.details')
-        ->with('success', 'Employee updated Successfully');
+            ->with('success', 'Employee updated Successfully');
     }
 }
